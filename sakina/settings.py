d1ceaaaaa31b23b461cccb3d2641ac0bc2a5cb7c
Sascha Klement <sascha.r.klement@gmail.com>
2026-05-17 15:42:29 +0200

    Move secret key to environment variables

--- sakina/settings.py
+++ sakina/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-g6j*2c7%#v33ec-r!7fa@(lx(jm0)@+)_)2k8n2a9rr-f#^23#'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
