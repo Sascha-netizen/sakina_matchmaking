d4c500031cb65fc356811682a36557681f5387f6
Sascha Klement <sascha.r.klement@gmail.com>
2026-05-18 17:58:45 +0200

    Register Profile model in admin with list display, list filter and search fields

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile
 
 # Register your models here.
+@admin.register(Profile)
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'gender', 'country', 'sect', 'completed', 'created_at')
+    list_filter = ('gender', 'sect', 'marital_status', 'completed', 'profile_visibility')
+    search_fields = ('user__username', 'country', 'city', 'ethnicity')
+
