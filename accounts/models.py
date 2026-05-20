b1969f7e299b71505ad501ed00df8cff7b3780a3
Sascha Klement <sascha.r.klement@gmail.com>
2026-05-20 13:48:56 +0200

    Cancel Stripe subscription on user deletion

--- accounts/models.py
+++ accounts/models.py
@@ -1,6 +1,20 @@
-from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.conf import settings
+import stripe
 
 
-class User (AbstractUser):
-    pass
+class User(AbstractUser):
+
+    def delete(self, *args, **kwargs):
+        from subscriptions.models import Subscription
+        stripe.api_key = settings.STRIPE_SECRET_KEY
+        for subscription in self.subscriptions.filter(
+            status=Subscription.Status.ACTIVE
+        ):
+            try:
+                stripe.Subscription.cancel(
+                    subscription.stripe_subscription_id
+                )
+            except stripe.error.StripeError:
+                pass
+        super().delete(*args, **kwargs)
